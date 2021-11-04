cc88cdc732fcefa3c538846e5a540d53efc8a014
Matt Straathof <matt.straat@gmail.com>
2021-11-03 18:36:56 -0700

    feat: add create cache unit test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use std::env;
+    use std::{env, thread::sleep, time::Duration};
 
     use momento::{response::cache_get_response::MomentoGetStatus, sdk::Momento};
     use uuid::Uuid;
@@ -10,13 +10,13 @@ mod tests {
         return Momento::new(auth_token).await.unwrap();
     }
 
-    fn get_cache_name() -> String {
+    fn get_shared_cache_name() -> String {
         return env::var("TEST_CACHE_NAME").expect("env var TEST_CACHE_NAME must be set");
     }
 
     #[tokio::test]
     async fn cache_miss() {
-        let cache_name = get_cache_name();
+        let cache_name = get_shared_cache_name();
         let cache_key = Uuid::new_v4().to_string();
         let mut mm = get_momento_instance().await;
         let cache = mm.get_cache(cache_name.as_str(), 10).await.unwrap();
@@ -26,7 +26,7 @@ mod tests {
 
     #[tokio::test]
     async fn cache_hit() {
-        let cache_name = get_cache_name();
+        let cache_name = get_shared_cache_name();
         let cache_key = Uuid::new_v4().to_string();
         let cache_body = Uuid::new_v4().to_string();
         let mut mm = get_momento_instance().await;
@@ -38,5 +38,39 @@ mod tests {
         let result = cache.get(cache_key.clone()).await.unwrap();
         assert!(matches!(result.result, MomentoGetStatus::HIT));
         assert_eq!(result.value, cache_body.as_bytes());
+    }
+
+    #[tokio::test]
+    async fn cache_respects_default_ttl() {
+        let cache_name = get_shared_cache_name();
+        let cache_key = Uuid::new_v4().to_string();
+        let cache_body = Uuid::new_v4().to_string();
+        let mut mm = get_momento_instance().await;
+        let cache = mm.get_cache(cache_name.as_str(), 1).await.unwrap();
+        cache
+            .set(cache_key.clone(), cache_body.clone(), None)
+            .await
+            .unwrap();
+        sleep(Duration::new(1, 0));
+        let result = cache.get(cache_key.clone()).await.unwrap();
+        assert!(matches!(result.result, MomentoGetStatus::MISS));
+    }
+
+    #[tokio::test]
+    async fn create_cache_then_set() {
+        let cache_name = Uuid::new_v4().to_string();
+        let cache_key = Uuid::new_v4().to_string();
+        let cache_body = Uuid::new_v4().to_string();
+        let mut mm = get_momento_instance().await;
+        mm.create_cache(&cache_name).await.unwrap();
+        let cache = mm.get_cache(&cache_name, 10).await.unwrap();
+        cache
+            .set(cache_key.clone(), cache_body.clone(), None)
+            .await
+            .unwrap();
+        let result = cache.get(cache_key.clone()).await.unwrap();
+        assert!(matches!(result.result, MomentoGetStatus::HIT));
+        assert_eq!(result.value, cache_body.as_bytes());
+        mm.delete_cache(&cache_name).await.unwrap();
     }
 }
