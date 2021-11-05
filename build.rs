bc862a34e0909f4643500bfce9337e55bed5dfe3
Matt Straathof <matt.straat@gmail.com>
2021-11-05 01:29:53 -0700

    chore: add placeholder build.rs file

--- build.rs
+++ build.rs
@@ -1,16 +1,27 @@
-use std::io::Result;
+// Uncomment this file when we want to generate new protos.
+// This is only here until we start publishing our protos.
+// This needs to stay commented out because when we `brew install`
+// the cli, rust is unable to build the protos. So we are building
+// them, and then commenting out the build file until we need to
+// rebuild them.
 
-fn main() -> Result<()> {
-    tonic_build::configure()
-        .build_server(false)
-        .out_dir("src/generated")
-        .compile(
-            &[
-                "src/protos/controlclient.proto",
-                "src/protos/cacheclient.proto",
-            ],
-            &["src/protos"],
-        )
-        .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
-    Ok(())
+// use std::io::Result;
+
+// fn main() -> Result<()> {
+//     tonic_build::configure()
+//         .build_server(false)
+//         .out_dir("src/generated")
+//         .compile(
+//             &[
+//                 "src/protos/controlclient.proto",
+//                 "src/protos/cacheclient.proto",
+//             ],
+//             &["src/protos"],
+//         )
+//         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+//     Ok(())
+// }
+
+fn main() {
+    println!("Placeholder build function");
 }
