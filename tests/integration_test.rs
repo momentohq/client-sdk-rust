6d8edba2b7311f9c0a6d27a6a0e5b896e963d39e
erikatharp <eem@momentohq.com>
2022-03-10 17:35:06 -0800

    add create and delete cache in each integraiton test and remove TEST_CACHE_NAME env variable

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -14,17 +14,15 @@ mod tests {
         return SimpleCacheClient::new(auth_token, 5).await.unwrap();
     }
 
-    fn get_shared_cache_name() -> String {
-        return env::var("TEST_CACHE_NAME").expect("env var TEST_CACHE_NAME must be set");
-    }
-
     #[tokio::test]
     async fn cache_miss() {
-        let cache_name = get_shared_cache_name();
+        let cache_name = Uuid::new_v4().to_string();
         let cache_key = Uuid::new_v4().to_string();
         let mut mm = get_momento_instance().await;
+        mm.create_cache(&cache_name).await.unwrap();
         let result = mm.get(&cache_name, cache_key).await.unwrap();
         assert!(matches!(result.result, MomentoGetStatus::MISS));
+        mm.delete_cache(&cache_name).await.unwrap();
     }
 
     #[tokio::test]
@@ -41,30 +39,34 @@ mod tests {
 
     #[tokio::test]
     async fn cache_hit() {
-        let cache_name = get_shared_cache_name();
+        let cache_name = Uuid::new_v4().to_string();
         let cache_key = Uuid::new_v4().to_string();
         let cache_body = Uuid::new_v4().to_string();
         let mut mm = get_momento_instance().await;
+        mm.create_cache(&cache_name).await.unwrap();
         mm.set(&cache_name, cache_key.clone(), cache_body.clone(), None)
             .await
             .unwrap();
         let result = mm.get(&cache_name, cache_key.clone()).await.unwrap();
         assert!(matches!(result.result, MomentoGetStatus::HIT));
         assert_eq!(result.value, cache_body.as_bytes());
+        mm.delete_cache(&cache_name).await.unwrap();
     }
 
     #[tokio::test]
     async fn cache_respects_default_ttl() {
-        let cache_name = get_shared_cache_name();
+        let cache_name = Uuid::new_v4().to_string();
         let cache_key = Uuid::new_v4().to_string();
         let cache_body = Uuid::new_v4().to_string();
         let mut mm = get_momento_instance().await;
+        mm.create_cache(&cache_name).await.unwrap();
         mm.set(&cache_name, cache_key.clone(), cache_body.clone(), None)
             .await
             .unwrap();
         sleep(Duration::new(1, 0)).await;
         let result = mm.get(&cache_name, cache_key.clone()).await.unwrap();
         assert!(matches!(result.result, MomentoGetStatus::MISS));
+        mm.delete_cache(&cache_name).await.unwrap();
     }
 
     #[tokio::test]
@@ -85,8 +87,11 @@ mod tests {
 
     #[tokio::test]
     async fn list_caches() {
+        let cache_name = Uuid::new_v4().to_string();
         let mut mm = get_momento_instance().await;
+        mm.create_cache(&cache_name).await.unwrap();
         mm.list_caches(None).await.unwrap();
+        mm.delete_cache(&cache_name).await.unwrap();
     }
 
     #[tokio::test]
