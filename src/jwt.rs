5ae804d94b41d609213830ce929620ae3a5767ae
gautamomento <89037104+gautamomento@users.noreply.github.com>
2022-07-20 13:53:02 -0700

    fix: Hosted Endpoint Construction and Claim validation (#63)
    * fix: Hosted Endpoint Construction and Claim validation
    
    1. Hosted endpoint construction was incomplete and didn't generate conrrect urls.
    2. The claim validation is based on the fields in `Claims`. So even if we require validation for `c` and `cp` claims, since these are Option<String> the validation succeeds. It required an additional check to enforce validation that `c` or `cp` claims must not be none when `momento_endpoint` is not specified.
    
    * add debug
    
    * Update src/endpoint_resolver.rs
    
    Co-authored-by: Ruth Linehan <1530016+rlinehan@users.noreply.github.com>
    
    * fix the jwt decode.
    
    * fix build
    
    Co-authored-by: Ruth Linehan <1530016+rlinehan@users.noreply.github.com>

--- src/jwt.rs
+++ src/jwt.rs
@@ -21,20 +21,32 @@ pub fn decode_jwt(jwt: &str, momento_endpoint: Option<String>) -> Result<Claims,
     validation.required_spec_claims.clear();
     validation.required_spec_claims.insert("sub".to_string());
 
-    // Global session tokens and new auth tokens will not contain these claims.
-    if momento_endpoint.is_none() {
-        validation.required_spec_claims.insert("c".to_string());
-        validation.required_spec_claims.insert("cp".to_string());
-    }
-
     validation.validate_exp = false;
     validation.insecure_disable_signature_validation();
+
     match decode(jwt, &key, &validation) {
-        Ok(token) => Ok(token.claims),
-        Err(_) => Err(MomentoError::ClientSdkError(
-            "Could not parse token. Please ensure a valid token was entered correctly.".to_string(),
-        )),
+        Ok(token) => {
+            let token_claims: Claims = token.claims;
+
+            // If Momento Endpoint is not provided, then `c` and `cp` claims must be present.
+            // If Momento Endpoint is present then that always takes precedence over the c and cp
+            // claims in the JWT, hence, there is no need to look for all possibilities.
+            if momento_endpoint.is_none() && (token_claims.c.is_none() || token_claims.cp.is_none())
+            {
+                log::debug!("Momento Endpoint is none and auth token is missing endpoints");
+                Err(token_parsing_error())
+            } else {
+                Ok(token_claims)
+            }
+        }
+        Err(_) => Err(token_parsing_error()),
     }
+}
+
+fn token_parsing_error() -> MomentoError {
+    MomentoError::ClientSdkError(
+        "Could not parse token. Please ensure a valid token was entered correctly.".to_string(),
+    )
 }
 
 #[cfg(test)]
@@ -72,5 +84,13 @@ mod tests {
         assert_eq!(claims.sub, "abcd");
         assert!(claims.c.is_none());
         assert!(claims.cp.is_none());
+    }
+
+    #[test]
+    fn invalid_no_c_cp_claims_jwt_with_no_momento_endpoint() {
+        let e = decode_jwt("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhYmNkIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.PTgxba", None).unwrap_err();
+        let _err_msg =
+            "Could not parse token. Please ensure a valid token was entered correctly.".to_owned();
+        assert!(matches!(e, MomentoError::ClientSdkError(_err_msg)));
     }
 }
