d75e81236745d72868a6de610e7a8ed3bd9413c3
Brian Martin <brayniac@gmail.com>
2022-12-19 11:33:23 -0800

    fix more clippy lint failures

--- src/endpoint_resolver.rs
+++ src/endpoint_resolver.rs
@@ -116,6 +116,7 @@ impl MomentoEndpointsResolver {
 }
 
 #[cfg(test)]
+#[allow(clippy::unwrap_used)]
 mod tests {
     use crate::endpoint_resolver::MomentoEndpointsResolver;
     use crate::response::error::MomentoError;
