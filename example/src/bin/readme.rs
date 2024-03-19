08ccd691d2579bf85415f8ef6270dba8a967c61f
Chris Price <chris@momentohq.com>
2024-03-18 20:54:20 -0700

    chore: fix Makefile build-examples, and get examples building

--- example/src/bin/readme.rs
+++ example/src/bin/readme.rs
@@ -1,32 +1,33 @@
-use std::time::Duration;
-use momento::{CacheClient, CredentialProvider, MomentoError};
-use momento::requests::cache::basic::get::Get;
 use momento::config::configurations::laptop;
+use momento::requests::cache::basic::get::Get;
+use momento::{CacheClient, CredentialProvider, MomentoError};
+use std::time::Duration;
 
 const CACHE_NAME: &str = "cache";
 
+#[tokio::main]
 pub async fn main() -> Result<(), MomentoError> {
     let cache_client = CacheClient::new(
         CredentialProvider::from_env_var("MOMENTO_API_KEY".to_string())?,
         laptop::latest(),
-        Duration::from_secs(60)
+        Duration::from_secs(60),
     )?;
 
     cache_client.create_cache(CACHE_NAME.to_string()).await?;
-    
-    match(cache_client.set(CACHE_NAME, "mykey", "myvalue").await) {
+
+    match cache_client.set(CACHE_NAME, "mykey", "myvalue").await {
         Ok(_) => println!("Successfully stored key 'mykey' with value 'myvalue'"),
-        Err(e) => println!("Error: {}", e)
+        Err(e) => println!("Error: {}", e),
     }
-    
-    let value: String = match(cache_client.get(CACHE_NAME, "mykey").await?) {
+
+    let value: String = match cache_client.get(CACHE_NAME, "mykey").await? {
         Get::Hit { value } => value.try_into().expect("I stored a string!"),
         Get::Miss => {
             println!("Cache miss!");
-            return Ok(()) // probably you'll do something else here
+            return Ok(()); // probably you'll do something else here
         }
     };
-    
+
     println!("Successfully retrieved value: {}", value);
 
     Ok(())
