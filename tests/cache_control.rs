e57d0517c9ca4c95143a8d3cfc431e83911b61db
anitarua <anita@momentohq.com>
2024-03-27 11:11:08 -0700

    remove alpha limits checking from list caches test

--- tests/cache_control.rs
+++ tests/cache_control.rs
@@ -31,45 +31,4 @@ async fn lists_existing_test_cache() {
         .map(|cache_info| cache_info.name.clone())
         .collect();
     assert!(cache_names.contains(&cache_name));
-
-    // Find test cache and check limits match expected for alpha cell
-    let cache_info = result
-        .caches
-        .iter()
-        .find(|cache_info| cache_info.name == cache_name)
-        .unwrap();
-
-    let expected_throughput_limit = 10240;
-    let expected_item_size_limit = 4883;
-    let expected_throttling_limit = 1000;
-    let expected_max_ttl = 86400;
-    let expected_publish_rate = 100;
-    let expected_subscription_count = 100;
-    let expected_publish_message_size = 100;
-
-    assert_eq!(
-        cache_info.cache_limits.max_throughput_kbps,
-        expected_throughput_limit
-    );
-    assert_eq!(
-        cache_info.cache_limits.max_item_size_kb,
-        expected_item_size_limit
-    );
-    assert_eq!(
-        cache_info.cache_limits.max_traffic_rate,
-        expected_throttling_limit
-    );
-    assert_eq!(cache_info.cache_limits.max_ttl_seconds, expected_max_ttl);
-    assert_eq!(
-        cache_info.topic_limits.max_publish_rate,
-        expected_publish_rate
-    );
-    assert_eq!(
-        cache_info.topic_limits.max_subscription_count,
-        expected_subscription_count
-    );
-    assert_eq!(
-        cache_info.topic_limits.max_publish_message_size_kb,
-        expected_publish_message_size
-    );
 }
