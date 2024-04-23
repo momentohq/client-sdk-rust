0f360db2e6b9ae76ecf49faf637cbb8902e26e75
Michael Landis <michael@momentohq.com>
2024-04-23 13:34:50 -0700

    docs: update contributing for tests and organize tests more (#230)
    Addresses PR comments here. Organizes the tests by service and documents integration test structure in the CONTRIBUTING file.

---
+++ tests/cache/mod.rs
@@ -0,0 +1,5 @@
+mod control;
+mod item;
+mod key_existence;
+mod scalar;
+mod sorted_set;
--- tests/old.rs
+++
@@ -1,4 +0,0 @@
-// In order to run tests in nested directories, they must be referenced here.
-mod old {
-    mod integration_test;
-}
