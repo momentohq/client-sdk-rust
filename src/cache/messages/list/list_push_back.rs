ace68b8315b47bbe9f393bc9a47c64b354ce607e
Anita Ruangrotsakun <138700973+anitarua@users.noreply.github.com>
2024-05-13 09:03:24 -0700

    feat: add list_push_front and list_push_back (#294)
    * feat: add list_push_front and list_push_back
    
    * fix doctests
    
    * rebase and fix errors

--- src/cache/messages/list/list_push_back.rs
+++ src/cache/messages/list/list_push_back.rs
@@ -1,18 +1,15 @@
 use crate::{
     cache::{CollectionTtl, MomentoRequest},
     utils::prep_request_with_timeout,
-    CacheClient, IntoBytes, IntoBytesIterable, MomentoResult,
+    CacheClient, IntoBytes, MomentoResult,
 };
 
-/// Adds multiple elements to the back of the given list. Creates the list if it does not already exist.
-///
-/// Appends the supplied list to the end of a list. For example, if you have a list [1, 2, 3]
-/// and listConcatenateBack [4, 5, 6], you will create [1, 2, 3, 4, 5, 6].
+/// Adds an element to the back of the given list. Creates the list if it does not already exist.
 ///
 /// # Arguments
 /// * `cache_name` - name of cache
 /// * `list_name` - name of the list
-/// * `values` - list of values to add to the back of the list
+/// * `value` - value to append to list
 ///
 /// # Optional Arguments
 ///
@@ -25,35 +22,35 @@ use crate::{
 /// # fn main() -> anyhow::Result<()> {
 /// # use momento_test_util::create_doctest_cache_client;
 /// # tokio_test::block_on(async {
-/// use momento::cache::{ListConcatenateBackResponse, ListConcatenateBackRequest, CollectionTtl};
+/// use momento::cache::{ListPushBackResponse, ListPushBackRequest, CollectionTtl};
 /// # let (cache_client, cache_name) = create_doctest_cache_client();
 /// let list_name = "list-name";
-/// let concat_back_request = ListConcatenateBackRequest::new(cache_name, list_name, vec!["value1", "value2"])
+/// let push_back_request = ListPushBackRequest::new(cache_name, list_name, "value1")
 ///     .ttl(CollectionTtl::default())
 ///     .truncate_front_to_size(10);
 ///
-/// match cache_client.send_request(concat_back_request).await {
-///     Ok(_) => println!("Elements added to list"),
-///     Err(e) => eprintln!("Error adding elements to list: {}", e),
+/// match cache_client.send_request(push_back_request).await {
+///     Ok(_) => println!("Element added to list"),
+///     Err(e) => eprintln!("Error adding element to list: {}", e),
 /// }
 /// # Ok(())
 /// # })
 /// # }
 /// ```
-pub struct ListConcatenateBackRequest<L: IntoBytes, V: IntoBytesIterable> {
+pub struct ListPushBackRequest<L: IntoBytes, V: IntoBytes> {
     cache_name: String,
     list_name: L,
-    values: V,
+    value: V,
     collection_ttl: Option<CollectionTtl>,
     truncate_front_to_size: Option<u32>,
 }
 
-impl<L: IntoBytes, V: IntoBytesIterable> ListConcatenateBackRequest<L, V> {
-    pub fn new(cache_name: impl Into<String>, list_name: L, values: V) -> Self {
+impl<L: IntoBytes, V: IntoBytes> ListPushBackRequest<L, V> {
+    pub fn new(cache_name: impl Into<String>, list_name: L, value: V) -> Self {
         Self {
             cache_name: cache_name.into(),
             list_name,
-            values,
+            value,
             collection_ttl: None,
             truncate_front_to_size: None,
         }
@@ -72,20 +69,20 @@ impl<L: IntoBytes, V: IntoBytesIterable> ListConcatenateBackRequest<L, V> {
     }
 }
 
-impl<L: IntoBytes, V: IntoBytesIterable> MomentoRequest for ListConcatenateBackRequest<L, V> {
-    type Response = ListConcatenateBackResponse;
+impl<L: IntoBytes, V: IntoBytes> MomentoRequest for ListPushBackRequest<L, V> {
+    type Response = ListPushBackResponse;
 
-    async fn send(self, cache_client: &CacheClient) -> MomentoResult<ListConcatenateBackResponse> {
+    async fn send(self, cache_client: &CacheClient) -> MomentoResult<ListPushBackResponse> {
         let collection_ttl = self.collection_ttl.unwrap_or_default();
-        let values = self.values;
+        let value = self.value.into_bytes();
         let list_name = self.list_name.into_bytes();
         let cache_name = &self.cache_name;
         let request = prep_request_with_timeout(
             cache_name,
             cache_client.configuration.deadline_millis(),
-            momento_protos::cache_client::ListConcatenateBackRequest {
+            momento_protos::cache_client::ListPushBackRequest {
                 list_name,
-                values: values.into_bytes(),
+                value,
                 ttl_milliseconds: cache_client.expand_ttl_ms(collection_ttl.ttl())?,
                 refresh_ttl: collection_ttl.refresh(),
                 truncate_front_to_size: self.truncate_front_to_size.unwrap_or(0),
@@ -95,11 +92,11 @@ impl<L: IntoBytes, V: IntoBytesIterable> MomentoRequest for ListConcatenateBackR
         let _ = cache_client
             .data_client
             .clone()
-            .list_concatenate_back(request)
+            .list_push_back(request)
             .await?;
-        Ok(ListConcatenateBackResponse {})
+        Ok(ListPushBackResponse {})
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct ListConcatenateBackResponse {}
+pub struct ListPushBackResponse {}
