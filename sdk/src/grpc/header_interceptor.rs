56401d8676abf9721cb7d579a6eb8b3fdf8baa36
Michael Landis <michael@momentohq.com>
2024-07-08 14:58:19 -0700

    chore: send sentinel value for `runtime-version` header (#381)
    Because rust does not have a runtime, we send a sentinel value rust
    for the header so that the server metrics at least have a stub value.
    
    Also simplifies header creation and insertion with utility functions. This
    reduces code duplication.

--- sdk/src/grpc/header_interceptor.rs
+++ sdk/src/grpc/header_interceptor.rs
@@ -1,4 +1,5 @@
 use std::convert::TryFrom;
+use std::str::FromStr;
 use std::sync::atomic::{AtomicBool, Ordering};
 
 #[derive(Clone)]
@@ -14,6 +15,18 @@ impl HeaderInterceptor {
             sdk_agent: sdk_agent.to_string(),
         }
     }
+
+    /// Insert a header into the request.
+    fn insert_header(
+        &self,
+        request: &mut tonic::Request<()>,
+        name: &str,
+        value: &str,
+    ) -> Result<(), tonic::Status> {
+        let (header_name, header_value) = create_header_from_string(name, value)?;
+        request.metadata_mut().insert(header_name, header_value);
+        Ok(())
+    }
 }
 
 impl tonic::service::Interceptor for HeaderInterceptor {
@@ -23,35 +36,42 @@ impl tonic::service::Interceptor for HeaderInterceptor {
     ) -> Result<tonic::Request<()>, tonic::Status> {
         static ARE_ONLY_ONCE_HEADER_SENT: AtomicBool = AtomicBool::new(false);
 
-        let auth_token =
-            tonic::metadata::AsciiMetadataValue::try_from(&self.auth_token).map_err(|e| {
-                tonic::Status::new(
-                    tonic::Code::InvalidArgument,
-                    format!("Couldn't parse auth token for auth header: {}", e),
-                )
-            })?;
-
-        request.metadata_mut().insert(
-            tonic::metadata::AsciiMetadataKey::from_static("authorization"),
-            auth_token,
-        );
-
-        let sdk_agent =
-            tonic::metadata::AsciiMetadataValue::try_from(&self.sdk_agent).map_err(|e| {
-                tonic::Status::new(
-                    tonic::Code::InvalidArgument,
-                    format!("Couldn't parse sdk agent for agent header: {}", e),
-                )
-            })?;
+        self.insert_header(&mut request, "authorization", &self.auth_token)?;
 
         if !ARE_ONLY_ONCE_HEADER_SENT.load(Ordering::Relaxed) {
-            request.metadata_mut().insert(
-                tonic::metadata::AsciiMetadataKey::from_static("agent"),
-                sdk_agent,
-            );
+            self.insert_header(&mut request, "agent", &self.sdk_agent)?;
+
+            // Because the `runtime-version` header makes more sense for interpreted languages,
+            // we send this sentinel value to ensure we report *some* value for this sdk.
+            self.insert_header(&mut request, "runtime-version", "rust")?;
             ARE_ONLY_ONCE_HEADER_SENT.store(true, Ordering::Relaxed);
         }
 
         Ok(request)
     }
+}
+
+fn create_header_from_string(
+    name: &str,
+    value: &str,
+) -> Result<
+    (
+        tonic::metadata::AsciiMetadataKey,
+        tonic::metadata::AsciiMetadataValue,
+    ),
+    tonic::Status,
+> {
+    let header_name = tonic::metadata::AsciiMetadataKey::from_str(name).map_err(|e| {
+        tonic::Status::new(
+            tonic::Code::InvalidArgument,
+            format!("Couldn't parse header name {}: {}", name, e),
+        )
+    })?;
+    let header_value = tonic::metadata::AsciiMetadataValue::try_from(value).map_err(|e| {
+        tonic::Status::new(
+            tonic::Code::InvalidArgument,
+            format!("Couldn't parse header value for {}: {}", name, e),
+        )
+    })?;
+    Ok((header_name, header_value))
 }
