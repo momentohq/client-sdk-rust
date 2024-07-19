e061a540e3b2f2e71c9e1917ec21321d7c0c90c5
anitarua <anita@momentohq.com>
2024-07-19 09:43:26 -0700

    WIP: use lambda_http crate to print api gateway proxy events

--- example/aws/cdk-lambda/lambda/momento-simple-lambda/src/main.rs
+++ example/aws/cdk-lambda/lambda/momento-simple-lambda/src/main.rs
@@ -1,49 +1,13 @@
-use std::time::Duration;
-use serde_json::Value;
-use lambda_runtime::{run, service_fn, tracing, Error, LambdaEvent};
-use momento::cache::configurations::Lambda;
-use momento::{CacheClient, CredentialProvider};
+use lambda_http::{run, service_fn, Error, IntoResponse, Request};
 
-const DEFAULT_TTL: Duration = Duration::from_secs(60);
-
-lazy_static::lazy_static! {
-    static ref CACHE_CLIENT: CacheClient = CacheClient::builder()
-    .default_ttl(DEFAULT_TTL)
-    .configuration(Lambda::latest())
-    .credential_provider(CredentialProvider::from_env_var("MOMENTO_API_KEY")
-        .expect("Unable to construct Momento CredentialProvider using env var MOMENTO_API_KEY"))
-    .build()
-    .expect("Unable to construct Momento CacheClient");
-}
-
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-/// - https://github.com/aws-samples/serverless-rust-demo/
-async fn function_handler(_event: LambdaEvent<Value>) -> Result<(), Error> {
+async fn function_handler(event: Request) -> Result<impl IntoResponse, std::convert::Infallible> {
     // Extract some useful information from the request
-
-    let set_result = CACHE_CLIENT.set("cache", "my-cache-key", "my-cache-value").await;
-    match set_result {
-        Ok(_) => println!("Successfully set cache value for key my-cache-key!"),
-        Err(e) => println!("Uh-oh. Failed to set cache key: {}", e),
-    }
-
-    let get_result: String = CACHE_CLIENT.get("cache", "my-cache-key")
-        .await
-        .expect("Failed to get cache value for key my-cache-key")
-        .try_into()
-        .expect("Failed to convert cache value to String");
-    
-    println!("Successfully retrieved cache value for key my-cache-key: {}", get_result);
-    
-    Ok(())
+    println!("event data payload: {:?}", event);
+    Result::<&str, std::convert::Infallible>::Ok("end of lambda")
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tracing::init_default_subscriber();
-
-    run(service_fn(function_handler)).await
+    run(service_fn(function_handler)).await?;
+    Ok(())
 }
