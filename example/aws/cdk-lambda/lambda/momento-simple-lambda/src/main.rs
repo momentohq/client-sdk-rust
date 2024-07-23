762ba0826e071e11cdadcd7b95680566627e4d25
anitarua <anita@momentohq.com>
2024-07-23 15:12:20 -0700

    WIP: use lambda_http to try to make momento call, send basic grpc response

--- example/aws/cdk-lambda/lambda/momento-simple-lambda/src/main.rs
+++ example/aws/cdk-lambda/lambda/momento-simple-lambda/src/main.rs
@@ -1,29 +1,47 @@
-use aws_lambda_events::{
-    apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse},
-    http::HeaderMap,
-};
 use base64::prelude::*;
-use lambda_http::lambda_runtime::{run, service_fn, Error, LambdaEvent};
+use lambda_http::{
+    http::{Response, StatusCode},
+    run, service_fn, Error, IntoResponse, Request,
+};
+use momento::{CacheClient, CredentialProvider};
+use std::time::Duration;
 
-async fn handler(
-    event: LambdaEvent<ApiGatewayProxyRequest>,
-) -> Result<ApiGatewayProxyResponse, Error> {
+async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
     println!("event data payload: {:?}", event);
 
-    let mut headers = HeaderMap::new();
-    headers.insert("Content-Type", "application/grpc+proto".parse().unwrap());
-    headers.insert("grpc-status", "0".parse().unwrap());
-    let resp = ApiGatewayProxyResponse {
-        status_code: 200,
-        multi_value_headers: headers.clone(),
-        is_base64_encoded: Some(true),
-        body: Some(aws_lambda_events::encodings::Body::Text(BASE64_STANDARD.encode("Hello AWS Lambda HTTP request"))),
-        headers,
-    };
-    Ok(resp)
+    let api_key = event.headers().get("authorization").unwrap().to_str()?;
+    println!("got api key: {:?}", api_key);
+
+    let creds = CredentialProvider::from_string(api_key);
+    match creds {
+        Ok(creds) => {
+            println!("creds: {:?}", creds);
+            let momento = CacheClient::builder()
+                .default_ttl(Duration::from_secs(60))
+                .configuration(momento::cache::configurations::Lambda::latest())
+                .credential_provider(creds)
+                .build()?;
+
+            let list_caches_resp = momento.list_caches().await?;
+            print!("list_caches_resp: {:?}", list_caches_resp);
+        }
+        Err(e) => {
+            println!("Error: {:?}", e);
+        }
+    }
+
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/grpc")
+        .header("grpc-status", 0)
+        .header("grpc-message", "Ok")
+        .body(BASE64_STANDARD.encode("Hello AWS Lambda HTTP request"))
+        .map_err(Box::new)?;
+
+    Ok(response)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    run(service_fn(handler)).await
+    run(service_fn(function_handler)).await
 }
