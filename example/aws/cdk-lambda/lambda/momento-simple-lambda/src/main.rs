6b0f4d428253f465e8296bc0ce6b1654c700afb2
anitarua <anita@momentohq.com>
2024-07-23 09:34:15 -0700

    WIP: try returning grpc response -- still receiving unknown content type error on client

--- example/aws/cdk-lambda/lambda/momento-simple-lambda/src/main.rs
+++ example/aws/cdk-lambda/lambda/momento-simple-lambda/src/main.rs
@@ -1,13 +1,29 @@
-use lambda_http::{run, service_fn, Error, IntoResponse, Request};
+use aws_lambda_events::{
+    apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse},
+    http::HeaderMap,
+};
+use base64::prelude::*;
+use lambda_http::lambda_runtime::{run, service_fn, Error, LambdaEvent};
 
-async fn function_handler(event: Request) -> Result<impl IntoResponse, std::convert::Infallible> {
-    // Extract some useful information from the request
+async fn handler(
+    event: LambdaEvent<ApiGatewayProxyRequest>,
+) -> Result<ApiGatewayProxyResponse, Error> {
     println!("event data payload: {:?}", event);
-    Result::<&str, std::convert::Infallible>::Ok("end of lambda")
+
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", "application/grpc+proto".parse().unwrap());
+    headers.insert("grpc-status", "0".parse().unwrap());
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        multi_value_headers: headers.clone(),
+        is_base64_encoded: Some(true),
+        body: Some(aws_lambda_events::encodings::Body::Text(BASE64_STANDARD.encode("Hello AWS Lambda HTTP request"))),
+        headers,
+    };
+    Ok(resp)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    run(service_fn(function_handler)).await?;
-    Ok(())
+    run(service_fn(handler)).await
 }
