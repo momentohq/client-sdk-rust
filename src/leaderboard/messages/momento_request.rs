2e155bd21c33ff9a887f04865841f73664ce968b
Michael Landis <michael@momentohq.com>
2025-02-05 10:58:28 -0800

    docs: clarify `MomentoRequest` documentation

--- src/leaderboard/messages/momento_request.rs
+++ src/leaderboard/messages/momento_request.rs
@@ -2,13 +2,11 @@ use crate::{Leaderboard, MomentoResult};
 
 /// A trait that allows Momento request types to define their interaction with the gRPC client.
 pub trait MomentoRequest {
-    #[allow(missing_docs)]
+    /// The response type for this request.
     type Response;
 
     /// An internal fn that allows Momento request types to define their interaction with
-    /// the gRPC client. You can impl this fn for your own types if you'd like to hand them
-    /// to the Momento client directly, but that is not an explicitly supported scenario and
-    /// this signature may change a little over time. If that's okay with you, impl away!
+    /// the gRPC client.
     #[doc(hidden)]
     fn send(
         self,
