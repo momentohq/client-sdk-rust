f9313094e3c88b493b9d8e8d33a14cbb17fd870c
anitarua <anita@momentohq.com>
2025-09-09 17:21:59 -0700

    fix lint error

--- src/protosocket/mod.rs
+++ src/protosocket/mod.rs
@@ -1,2 +1,2 @@
-/// Contains the [ProtosocketCacheClient] for interacting with Momento Cache using the Protosocket protocol.
+/// Contains the ProtosocketCacheClient for interacting with Momento Cache using the Protosocket protocol.
 pub mod cache;
