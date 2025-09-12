5f67ec0df583f66b8fe786a2cabd118fe2dcd023
anitarua <anita@momentohq.com>
2025-09-12 16:06:55 -0700

    should defer installing CryptoProvider default to caller

--- src/protosocket/cache/cache_client_builder.rs
+++ src/protosocket/cache/cache_client_builder.rs
@@ -17,7 +17,6 @@ use rustls_pki_types::ServerName;
 use std::convert::TryFrom;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::time::Duration;
-use tokio_rustls::rustls::crypto::aws_lc_rs::default_provider;
 
 pub type Serializer = ProstSerializer<CacheResponse, CacheCommand>;
 
@@ -184,9 +183,6 @@ impl ProtosocketCacheClientBuilder<ReadyToBuild> {
                 MomentoError::unknown_error("build", Some(e.to_string()))
             })?;
 
-        tokio_rustls::rustls::crypto::CryptoProvider::install_default(default_provider())
-            .map_err(|_| MomentoError::unknown_error("build", None))?;
-
         match self.0.credential_provider.endpoint_security {
             EndpointSecurity::Tls => {
                 // TODO: use a default value or panic here?
@@ -195,8 +191,15 @@ impl ProtosocketCacheClientBuilder<ReadyToBuild> {
                     .next()
                     .unwrap_or("localhost")
                     .to_string();
-                let server_name = ServerName::try_from(hostname)
-                    .map_err(|_| MomentoError::unknown_error("build", None))?;
+                let server_name = ServerName::try_from(hostname.clone()).map_err(|_| {
+                    MomentoError::unknown_error(
+                        "build",
+                        Some(format!(
+                            "Error creating server name from hostname: {}",
+                            hostname
+                        )),
+                    )
+                })?;
                 let with_tls = WebpkiTlsStreamConnector::new(server_name);
                 let (client, connection) = protosocket_rpc::client::connect::<
                     Serializer,
@@ -226,8 +229,15 @@ impl ProtosocketCacheClientBuilder<ReadyToBuild> {
                     .next()
                     .unwrap_or("localhost")
                     .to_string();
-                let server_name = ServerName::try_from(hostname)
-                    .map_err(|_| MomentoError::unknown_error("build", None))?;
+                let server_name = ServerName::try_from(hostname.clone()).map_err(|_| {
+                    MomentoError::unknown_error(
+                        "build",
+                        Some(format!(
+                            "Error creating server name from hostname: {}",
+                            hostname
+                        )),
+                    )
+                })?;
                 let with_tls = UnverifiedTlsStreamConnector::new(server_name);
                 let (client, connection) = protosocket_rpc::client::connect::<
                     Serializer,
