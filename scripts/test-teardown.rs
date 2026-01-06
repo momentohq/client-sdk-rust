4fa6134fc295af31a236499400d871483256c9f8
Nate Anderson <nate@momentohq.com>
2026-01-06 13:37:09 -0800

    Create and delete the auth cache in test setup scripts
    Align the styles of the test setup scripts.

--- scripts/test-teardown.rs
+++ scripts/test-teardown.rs
@@ -1,26 +1,29 @@
 use std::time::Duration;
 
 use momento::cache::configurations;
-use momento::CacheClient;
-use momento_test_util::{get_test_cache_name, get_v1_test_credential_provider};
+use momento::{CacheClient, MomentoResult};
+use momento_test_util::{
+    get_test_auth_cache_name, get_test_cache_name, get_v1_test_credential_provider,
+};
 
 #[tokio::main]
-#[allow(clippy::expect_used)] // we want to panic if teardown cannot complete
-async fn main() {
+async fn main() -> MomentoResult<()> {
     let cache_name = get_test_cache_name();
+    let auth_cache_name = get_test_auth_cache_name();
 
     let credential_provider = get_v1_test_credential_provider();
 
     let cache_client = CacheClient::builder()
         .default_ttl(Duration::from_secs(5))
         .configuration(configurations::Laptop::latest())
-        .credential_provider(credential_provider.clone())
-        .build()
-        .expect("cache client cannot be created");
+        .credential_provider(credential_provider)
+        .build()?;
 
-    println!("Deleting cache {}", cache_name.clone());
-    cache_client
-        .delete_cache(cache_name.clone())
-        .await
-        .expect("cache cannot be deleted");
+    println!("Deleting test cache {}", cache_name);
+    cache_client.delete_cache(cache_name).await?;
+
+    println!("Deleting auth test cache {}", auth_cache_name);
+    cache_client.delete_cache(auth_cache_name).await?;
+
+    Ok(())
 }
